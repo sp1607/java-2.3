52eeaae2ada188bd2d351356aac030353e8487b2
sp1607 <77290138+sp1607@users.noreply.github.com>
2021-02-08 13:09:08 +0300

    Initial commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,91 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateForNotRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateForNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndAmount0() {
+        BonusService service = new BonusService();
+
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateForNotRegisteredAndAmount0() {
+        BonusService service = new BonusService();
+
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+}
